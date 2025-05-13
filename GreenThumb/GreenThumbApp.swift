f99b83130891f0241dc5ebbd274ed4998d0908a8
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2025-05-13 20:58:29 +0900

    first commit

---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by 川口美咲 on 2025/05/13.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
