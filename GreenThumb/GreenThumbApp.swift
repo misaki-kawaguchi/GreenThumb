c3e3e3e9d9c69e6d87f2dd012f49bdc341859278
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2025-05-24 14:35:03 +0900

    Tab Barを表示する

--- GreenThumb/GreenThumbApp.swift
+++ GreenThumb/GreenThumbApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct GreenThumbApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                VegetableListScreen()
-            }
+            VegetableTabBarScreen()
         }
     }
 }
